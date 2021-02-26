b56972408bbf3a99c35b0d269d477dbd893a1e7c
Hooyar <hooyarsh@gmail.com>
2021-02-26 14:59:42 +0330

    rickroll

---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+print ("your name")
+a = input()
+x = os.popen("ffplay './.test/Rick Astley - Never Gonna Give You Up [HQ]-DLzxrzFCyOs.webm' > /dev/null 2>&1")
+print ("You've been RickRolled " + a )
